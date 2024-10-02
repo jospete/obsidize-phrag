c6c259f0bff782dcc8cd800fb1106d2eb4aa6e0b
= <obsidize@gmail.com>
2024-10-01 22:02:15 -0500

    add pwa capability to react variant

--- projects/phrag-react/vite.config.ts
+++ projects/phrag-react/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 	},
-	plugins: [
-		react()
-	],
 	esbuild: {
 		legalComments: 'none'
 	},
@@ -18,5 +16,41 @@ export default defineConfig({
 				comments: false
 			}
 		}
-	}
+	},
+	plugins: [
+		react(),
+		VitePWA({ 
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			manifest: {
+			  name: 'Obsidize (Phra)se (G)enerator',
+			  short_name: 'Obsidize Phrag',
+			  theme_color: '#555555',
+			  icons: [
+				{
+					src: 'pwa-64x64.png',
+					sizes: '64x64',
+					type: 'image/png'
+				},
+				{
+					src: 'pwa-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any'
+				},
+				{
+					src: 'maskable-icon-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'maskable'
+				}
+			  ],
+			}, 
+		})
+	],
 });

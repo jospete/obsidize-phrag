6c73564db9e84dab07d8f42f5336cd6c31ccbaea
= <obsidize@gmail.com>
2024-10-01 22:35:26 -0500

    update vite config to maaaayyybeee fix this stupid github pages issue

--- projects/phrag-react/vite.config.ts
+++ projects/phrag-react/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/obsidize-phrag/',
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 	},
